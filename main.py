e000105180bc9352dc326bba2ced08ba512da00c
Anton60hp <vladger2001@mail.ru>
2023-09-05 10:24:10 +0300

    Added Text and player size scaling just in case

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import arcade
 
-# Const
+## Constant defaults
+
+# Window constants
 GAME_WIDTH = 1600
 GAME_HEIGHT = 800
 GAME_TITLE = "Arcade Game"
+# Text constants
+LINE_HEIGHT = 40
+FONT_SIZE = 20
 # Player movement
 PLAYER_MS = 5
 PLAYER_JUMP_SPEED = 20
@@ -11,8 +16,9 @@ GRAVITY = 1
 
 # Scaling
 TILE_SCALING = 0.5
+PLAYER_SCALING = 1
 
-class GameGem (arcade.Window):
+class SomeGame (arcade.Window):
 
     def __init__(self): 
 
@@ -27,9 +33,22 @@ class GameGem (arcade.Window):
         arcade.set_background_color(arcade.color.BABY_BLUE_EYES)
 
     def setup(self):
-        
+
+        text_x = LINE_HEIGHT / 2 
+        text_y = GAME_HEIGHT - LINE_HEIGHT
+        self.fullscreenText = arcade.Text(
+            "Press F11 to togle fullscreen. Use WASD to move.",
+            text_x,
+            text_y,
+            arcade.color.BLACK,
+            FONT_SIZE * 1.5,
+            width=GAME_WIDTH
+        )
+
+        # Camera
         self.camera = arcade.Camera(self.width, self.height)
 
+        # Scene
         self.scene = arcade.Scene()
         self.scene.add_sprite_list("Player")
         self.scene.add_sprite_list("Walls", use_spatial_hash=True)
@@ -38,7 +57,7 @@ class GameGem (arcade.Window):
 
         # Character
         image_source = ":resources:images/animated_characters/robot/robot_idle.png"
-        self.player_sprite = arcade.Sprite(image_source)
+        self.player_sprite = arcade.Sprite(image_source, PLAYER_SCALING)
         self.player_sprite.center_x = 64
         self.player_sprite.center_y = 128
         self.scene.add_sprite("Player", self.player_sprite)
@@ -61,7 +80,6 @@ class GameGem (arcade.Window):
         for x in range (288, 480, 64):
             coordinate_list.append([x,160])
         
-
         for coordinate in coordinate_list:
             wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", TILE_SCALING)
             wall.position = coordinate
@@ -70,13 +88,15 @@ class GameGem (arcade.Window):
         # Physics Engine
         self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite, gravity_constant=GRAVITY, walls=self.scene["Walls"])
 
-
+    
 
     def on_draw(self):
 
         self.clear()
-
+        
         self.scene.draw()
+
+        self.fullscreenText.draw()
 
         self.camera.use()
 
@@ -92,6 +112,7 @@ class GameGem (arcade.Window):
         if key == arcade.key.W or key == arcade.key.SPACE:
             if self.physics_engine.can_jump():
                 self.player_sprite.change_y = PLAYER_JUMP_SPEED
+        
         # Player movement
         elif key == arcade.key.A:
             self.player_sprite.change_x = -PLAYER_MS
@@ -130,7 +151,7 @@ class GameGem (arcade.Window):
 
 def main():
 
-    window = GameGem()
+    window = SomeGame()
     window.setup()
     arcade.run()
 
